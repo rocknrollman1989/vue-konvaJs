c1500cd727c5004691093e1ec72e02454568d9de
RocknrollMan <rocknrollman1989@gmail.com>
2019-07-10 13:00:16 +0300

    add view info modal

--- src/components/charts/RectangleChart.vue
+++ src/components/charts/RectangleChart.vue
@@ -2,6 +2,11 @@
 
     <template>
       <div class="chart__container">
+        <!-- modals -->
+        <div :style="`top: ${positionTop - 40}px; left: ${positionLeft + 5}px; display: ${displayInfoModal ? 'block' : 'none'}`" class="chart__container__viewInfoModal">
+          <ViewChartsValueModal :enter-value-to-view="valueToView"/>
+        </div>
+        <!--  -->
         <v-stage :config="konvaInitObject.configKonva">
           <v-layer>
             <v-line :config="konvaInitObject.axisYline"></v-line>
@@ -13,28 +18,67 @@
               <v-text :config="graduate.textConfig" :key="graduate.id"></v-text>
             </template>
             <template v-for="(rectangle) in konvaInitObject.rectangles">
-              <v-rect :config="rectangle.rectConfig" :key="rectangle.id"></v-rect>
+              <v-rect :config="rectangle.rectConfig" :key="rectangle.id"
+              @mouseover="setChartInfo(rectangle.rectConfig.inputValue)"
+              @mousemove="viewChartInfo($event.evt)"
+              @mouseleave="hideChartInfo"
+              ></v-rect>
             </template>
           </v-layer>
-        </v-stage>
+        </v-stage> 
       </div>
     </template>
     
-    <script>
+<script>
 import { initRectangleChart } from '@/helpers';
+import ViewChartsValueModal from '@/components/modals/ViewChartsValueModal.vue';
+
 
 export default {
+  components: {
+    ViewChartsValueModal,
+  },
   props: {
     chartData: Object,
   },
   data() {
     return {
       konvaInitObject: {},
+      valueToView: 12345,
+      displayInfoModal: false,
+      positionTop: null,
+      positionLeft: null, 
     };
   },
   mounted() {
     this.konvaInitObject = { ...initRectangleChart(this.chartData) };
+  },
+  methods: {
+    viewChartInfo(e) {
+      this.displayInfoModal = true;
+      this.positionTop = e.offsetY;
+      this.positionLeft = e.offsetX;
+    },
+    setChartInfo(value) {
+      this.valueToView = value;
+    },
+    hideChartInfo() {
+      this.displayInfoModal = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.chart__container {
+  position: relative;
+
+  &__viewInfoModal {
+    position: absolute;
+    z-index: 100;
   }
 }
-</script>
+</style>
+
     
+  
---
+++ src/components/modals/ViewChartsValueModal.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border border-success bg-primary p-1 d-flex justify-content-center">
+    <span style="color: #14fddf; font-size: 16px;">{{enterValueToView}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['enterValueToView'],
+}
+</script>
